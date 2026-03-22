f42ca37f9004d47cb183084e7e08231a9e38f77b
lakmaldhanu87-hub <lakmaldhanu87@gmail.com>
2026-03-22 17:30:13 +0530

    add firebase connection

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_8p2hklBwCAYD_f8MH5A3q3IXE64iESc",
+  authDomain: "factory-erp-380a4.firebaseapp.com",
+  projectId: "factory-erp-380a4",
+  storageBucket: "factory-erp-380a4.firebasestorage.app",
+  messagingSenderId: "60731657812",
+  appId: "1:60731657812:web:ef67bf6817c5f7af2c27c1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
